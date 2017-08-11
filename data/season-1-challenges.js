ced887fb4a5ee2b1736723fe8574d510dcd32a5c
Steven McConomy <srmccono@uwaterloo.ca>
2017-08-11 19:33:49 -0400

    whoop

--- data/season-1-challenges.js
+++ data/season-1-challenges.js
@@ -1,14 +1,191 @@
-module.exports = [
-  {
-    title: 'The Skulltula Hunt',
-    rules: 'images/Challenges/Challenge1.png',
-    clue: null,
-    date: 'September 18, 2016 at 3:00 PM ET',
-    winner: {
-      name: 'Amateseru',
-      link: 'https://www.twitch.tv/amateseru',
-    },
-    time: '1:37:29',
-    recap: '/recap1',
-  }
-]
+module.exports = {
+  main: [
+    {
+      title: 'The Skulltula Hunt',
+      rules: 'images/Challenges/Challenge1.png',
+      clue: null,
+      date: 'September 18, 2016',
+      winner: {
+        name: 'Amateseru',
+        link: 'https://www.twitch.tv/amateseru',
+      },
+      time: '1:37:29',
+      recap: '/recap1',
+      results: 'http://www.speedrunslive.com/races/result/#!/180407',
+      background: '/images/Gold-Skulltula-2.png',
+    }, {
+      title: 'Glass Ganon',
+      rules: 'images/Challenges/Challenge2.png',
+      clue: '/clue/clue2.mp3',
+      date: 'October 2, 2016',
+      winner: {
+        name: 'Sniping',
+        link: 'https://www.twitch.tv/sniping117',
+      },
+      time: '1:47:12',
+      recap: '/recap2',
+      results: 'http://www.speedrunslive.com/races/result/#!/181301',
+      background: '/images/340px-Ganondorf.png',
+    }, {
+      title: 'Killink Spree!',
+      rules: 'images/Challenges/Challenge3.png',
+      clue: '/clue/Clue3.html',
+      date: 'October 16, 2016',
+      winner: {
+        name: 'Exodus',
+        link: 'https://www.twitch.tv/exodus122',
+      },
+      time: '1:40:16',
+      recap: '/recap3',
+      results: 'http://www.speedrunslive.com/races/result/#!/182117',
+      background: '/images/461px-OoT_Ganon.png',
+    }, {
+      title: 'Trick or Treat',
+      rules: 'images/Challenges/Challenge4.png',
+      clue: '/clue/Clue4.mp3',
+      date: 'October 30, 2016',
+      winner: {
+        name: 'Sniping',
+        link: 'https://www.twitch.tv/sniping117',
+      },
+      time: '1:29:43',
+      recap: '/recap4',
+      results: 'http://www.speedrunslive.com/races/result/#!/182935',
+      background: '/images/Link_MM_25Anny.png',
+    }, {
+      title: '<del>MS</del>T',
+      rules: 'images/Challenges/Challenge5.png',
+      clue: '/clue5',
+      date: 'November 13, 2016',
+      winner: {
+        name: 'Marco',
+        link: 'https://www.twitch.tv/marco8641',
+      },
+      time: '1:27:19',
+      recap: '/recap5',
+      results: 'http://www.speedrunslive.com/races/result/#!/183772',
+      background: '/images/322px-OoT_Link_Using_Fairy_Bow_Artwork.png',
+    }, {
+      title: 'The Janitor Challenge',
+      rules: 'images/Challenges/Challenge6.png',
+      clue: '/clue/Clue6.png',
+      date: 'November 27, 2016',
+      winner: {
+        name: 'Tob3000',
+        link: 'https://www.twitch.tv/tob3000',
+      },
+      time: '1:26:11',
+      recap: '/recap6',
+      results: 'http://www.speedrunslive.com/races/result/#!/184600',
+      background: '/images/415px-OoT_Talon_art.png',
+    }, {
+      title: 'The Downside? ...',
+      rules: 'images/Challenges/Challenge7.png',
+      clue: '/clue/Clue7.mp4',
+      date: 'December 11, 2016',
+      winner: {
+        name: 'Runnerguy',
+        link: 'https://www.twitch.tv/runnerguy2489',
+      },
+      time: '1:09:50',
+      recap: '/recap7',
+      results: 'http://www.speedrunslive.com/races/result/#!/185456',
+      background: '/images/464px-OoT_Stalfos_Artwork.png',
+    }, {
+      title: 'A Kakariko Kristmas',
+      rules: 'images/Challenges/Challenge8.png',
+      clue: '/clue8',
+      date: 'December 18, 2016',
+      winner: {
+        name: 'Exodus',
+        link: 'https://www.twitch.tv/exodus122',
+      },
+      time: '1:21:41',
+      recap: '/recap8',
+      results: 'http://www.speedrunslive.com/races/result/#!/185837',
+      background: '/images/640px-OoT_Link_Riding_Epona_Artwork.png',
+    }, {
+      title: 'The Materialist',
+      rules: 'images/Challenges/Challenge9.png',
+      clue: '/clue9',
+      date: 'January 22, 2017',
+      winner: {
+        name: 'Exodus',
+        link: 'https://www.twitch.tv/exodus122',
+      },
+      time: '1:27:42',
+      recap: '/recap9',
+      results: 'http://www.speedrunslive.com/races/result/#!/187615',
+      background: '/images/Link_Treasure_OoT.png',
+    }, {
+      title: 'OCS Bingo',
+      rules: 'images/Challenges/Challenge10.png',
+      clue: '/clue/Clue10.mp4',
+      date: 'February 5, 2017',
+      winner: {
+        name: 'Marco',
+        link: 'https://www.twitch.tv/marco8641',
+      },
+      time: '1:28:04',
+      recap: '/recap10',
+      results: 'http://www.speedrunslive.com/races/result/#!/188647',
+      background: '/images/Link_Block_OoT.png',
+    }
+  ],
+  playoffs: [
+    {
+      title: 'Child Only',
+      rules: 'images/Challenges/Challenge11.png',
+      clue: '/clue/Clue11.png',
+      date: 'February 19, 2017',
+      winner: {
+        name: 'Tob3000',
+        link: 'https://www.twitch.tv/tob3000',
+      },
+      loser: {
+        name: 'Marco',
+        link: 'https://www.twitch.tv/marco8641',
+      },
+      background: '/images/800px-MM_Link_3.png',
+    }, {
+      title: 'Skulltula Lockout',
+      rules: 'images/Challenges/Challenge12.png',
+      date: 'March 5, 2017',
+      winner: {
+        name: 'Exodus',
+        link: 'https://www.twitch.tv/exodus122',
+      },
+      loser: {
+        name: 'Sniping',
+        link: 'https://www.twitch.tv/sniping117',
+      },
+      background: '/images/Gold_Skulltula.png',
+    }, {
+      title: '6 Bosses',
+      rules: 'images/Challenges/Challenge13.png',
+      date: 'March 25, 2017',
+      winner: {
+        name: 'Exodus',
+        link: 'https://www.twitch.tv/exodus122',
+      },
+      loser: {
+        name: 'Tob3000',
+        link: 'https://www.twitch.tv/tob3000',
+      },
+      background: '/images/800px-Gohma.png',
+    }, {
+      title: 'Hyrule Fieldnalist',
+      rules: 'images/Challenges/Challenge14.png',
+      date: 'April 2, 2017',
+      winner: {
+        name: 'Exodus',
+        link: 'https://www.twitch.tv/exodus122',
+      },
+      loser: {
+        name: 'Hyperion',
+        link: 'https://www.twitch.tv/hyperion64',
+      },
+      background: '/images/728px-Volvagia.png',
+    }
+  ],
+}
