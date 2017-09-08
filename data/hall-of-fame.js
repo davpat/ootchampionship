367c609755302ba55f4292e65a46d4b42adfbbb0
unknown <zeldavslink@gmail.com>
2017-09-08 14:06:24 -0400

    added missing data

---
+++ data/hall-of-fame.js
@@ -0,0 +1,384 @@
+module.exports = {
+  Recent: 
+  [
+  	{
+		title: 'Dungeonfire Puzzle',
+		image: '/oldpuzzle/Dungeonfire.png',
+		creator: 'Dungeonfire',
+    },
+	{
+		title: 'Link Vacantion Album',
+		image: '/oldpuzzle/gombilPreOcS2.png',
+		creator: 'Gombill',
+    },
+	{
+		title: 'Meme Mastery',
+		image: 'oldpuzzle/Meme.png',
+		creator: 'Jolin012',
+    }
+  ],
+  main: [
+  	{
+		title: 'Dont lose your way',
+		image: 'oldpuzzle/sydney.jpg',
+		creator: 'VerySydney',
+    },
+  	{
+		title: 'Bomb Limit',
+		image: 'oldpuzzle/BombLimit.png',
+		creator: 'DankMoose',
+    },
+	{
+		title: 'Puzzle Challenge',
+		image: 'oldpuzzle/operadics.png',
+		creator: 'OOperadics',
+    },
+	{
+		title: 'Water Temple Goal',
+		image: 'oldpuzzle/WaterTempleGoal.png',
+		creator: '',
+    },
+	{
+		title: 'Defeat One Flare Dancer',
+		image: 'oldpuzzle/ChrisbullcockPuzzle.jpg',
+		creator: 'Chrisbullcock',
+    },
+	{
+		title: 'Have A Total of 10 Hearts',
+		image: 'oldpuzzle/Jenslang.jpg',
+		creator: 'Jenslang',
+    },
+	{
+		title: 'Defeat Amy',
+		image: 'oldpuzzle/DefeatAmy.jpg',
+		creator: '',
+    },
+	{
+		title: 'Obtain Biggoron Sword',
+		image: 'oldpuzzle/ObtainBiggoronSword.jpg',
+		creator: 'WBS64118771 & Bakeneko1048',
+    },
+	{
+		title: 'Dispel Forest Trial',
+		image: 'oldpuzzle/DisspelForestTrial.jpg',
+		creator: '',
+    },
+	{
+		title: 'Barinade and Ruto Letter on C',
+		image: 'oldpuzzle/BarinadeandLetterC.png',
+		creator: 'pokey',
+    },
+	{
+		title: 'Scenary Puzzle (Go To Location)',
+		image: 'oldpuzzle/ScenaryPuzzle(GoToLocation).jpg',
+		creator: '',
+    },
+	{
+		title: 'OoT, Lttp and MM Riddles',
+		image: 'oldpuzzle/Riddle.jpg',
+		creator: 'Adarax',
+    },
+	{
+		title: 'Ratio Keys Puzzle',
+		image: 'oldpuzzle/OoTPuzzleRaceWhatGoingOn.png',
+		creator: 'Wodahs-Reklaw',
+    },
+	{
+		title: 'NPC Puzzle',
+		image: 'oldpuzzle/OoTPuzzleRaceNPC.jpg',
+		creator: 'Gutiger',
+    },
+	{
+		title: 'Get Nocturne of Shadow',
+		image: 'oldpuzzle/OoTPuzzleRaceAma.jpg',
+		creator: 'Amateseru',
+    },
+	{
+		title: 'Defeat Meg',
+		image: 'oldpuzzle/IronBootandBrokenKnife.jpg',
+		creator: '',
+    },
+	{
+		title: 'Creep and Capture',
+		image: 'oldpuzzle/Fairy(DK64%20and%20OoT).png',
+		creator: 'Wodahs-Reklaw',
+    },
+	{
+		title: 'Beat Water Temple',
+		image: 'oldpuzzle/Exodus2.png',
+		creator: 'Exodus',
+    },
+	{
+		title: 'Iventory Puzzle',
+		image: 'oldpuzzle/ChallengeGoalJarvitz.jpg',
+		creator: 'Wodahs-Reklaw',
+    },
+  ],
+  DannyB: 
+  [
+  	{
+		title: 'Serving Hard Time',
+		image: 'oldpuzzle/ServingHardTime.png',
+		creator: 'DannyB21892',
+    },
+	{
+		title: 'Heartbreaker',
+		image: 'oldpuzzle/Heartbreaker.png',
+		creator: 'DannyB21892',
+    },
+	{
+		title: 'From Rags to Riches to Rags',
+		image: 'oldpuzzle/FromRagstoRichestoRags.png',
+		creator: 'DannyB21892',
+    },
+	{
+		title: 'A Lofty Goal',
+		image: 'oldpuzzle/ALoftyGoal.png',
+		creator: 'DannyB21892',
+    },
+	{
+		title: 'Redead Revenge',
+		image: 'oldpuzzle/ReDeadReVenge.png',
+		creator: 'DannyB21892',
+    }
+  ],
+  Mrbubbleskp: 
+  [
+  	{
+		title: 'Defeat TwinRova with the Biggoron Sword!',
+		image: 'oldpuzzle/MrBubbleskp1.jpg',
+		creator: 'Mrbubbleskp',
+    },
+	{
+		title: 'Dispel the Trial Barrier',
+		image: 'oldpuzzle/Mrbubbleskp2.jpg',
+		creator: 'Mrbubbleskp',
+    }
+  ],
+  Holidays: 
+  [
+  	{
+		title: 'Halloween Race',
+		image: 'oldpuzzle/CosmoHallowen.jpg',
+		creator: 'Cosmo',
+    },
+	{
+		title: 'New Year Puzzle',
+		image: 'oldpuzzle/NewYearPuzzle.png',
+		creator: 'Hyperion64 & Natalyahasdied',
+    },
+	{
+		title: 'Easter Puzzle',
+		image: 'oldpuzzle/EasterPuzzle.png',
+		creator: '',
+    },
+  ],
+  Sniping: 
+  [
+
+  	{
+		title: 'Beat Shadow Temple',
+		image: 'oldpuzzle/Snipping1.png',
+		creator: 'SnipingG117',
+    },
+  	{
+		title: 'Obtain Both Frog HP',
+		image: 'oldpuzzle/Snipping2.png',
+		creator: 'SnipingG117',
+    }
+  ],
+  PsyMarth: 
+  [
+	{
+		title: 'Borrow the Keaton Mask from the Happy Mask Shop',
+		image: 'oldpuzzle/PsyMarth1.png',
+		creator: 'PsyMarth',
+    },
+	{
+		title: 'Obtain all 3 Fairy Spells & all 3 Elemental Arrows',
+		image: 'oldpuzzle/PsyMarth2.png',
+		creator: 'PsyMarth',
+    },
+	{
+		title: 'Obtain the Fairy Slingshot!',
+		image: 'oldpuzzle/PsyMarth3.png',
+		creator: 'PsyMarth',
+    },
+  ],
+  MP16: 
+  [
+  	{
+		title: 'Light Torches Shadow Temple',
+		image: 'oldpuzzle/LightTorchesShadowTemple.png',
+		creator: 'MP16',
+    },
+	{
+		title: 'Restore link courage',
+		image: 'oldpuzzle/PuzzleMP16.png',
+		creator: 'MP16',
+    },
+  ],
+  OoTPuzzleRaceSerie: 
+  [
+  	{
+		title: 'Empty the equipment subscreen except for the Kokiri Boots.',
+		image: '',
+		creator: 'Cosmo',
+		number: '1'
+    },
+	{
+		title: 'Beat water Temple.',
+		image: 'oldpuzzle/OoTPuzzleRace3.jpg',
+		creator: 'Runnerguy2489',
+		number: '3'
+    },
+	{
+		title: 'Get Golden Skulltula token to your Deku Tree Dungeon Map Screen.',
+		image: 'oldpuzzle/OoTPuzzleRace4.png',
+		creator: 'Kazooie & Librari',
+		number: '4'
+    },
+	{
+		title: 'Open the Ice Arrow chest.',
+		image: 'oldpuzzle/OoTPuzzleRace5.png',
+		creator: 'Cosmo',
+		number: '5'
+    },
+	{
+		title: 'Obtain the Empty Bottle from Poe Collector.',
+		image: 'oldpuzzle/OoTPuzzleRace6.png',
+		creator: 'Exodus',
+		number: '6'
+    },
+	{
+		title: 'Obtain the Fire Medallion',
+		image: 'oldpuzzle/OoTPuzzleRace7.png',
+		creator: 'Sockfolder',
+		number: '7'
+    },
+	{
+		title: 'Enter x blue warp',
+		image: 'oldpuzzle/OoTPuzzleRace8.png',
+		creator: 'Enterim',
+		number: '8'
+    },
+	{
+		title: 'Obtain Spiritual Stones',
+		image: 'oldpuzzle/OoTPuzzleRace9.jpg',
+		creator: 'Samura1Man',
+		number: '9'
+    },
+	{
+		title: 'Beat 3 dungeon',
+		image: 'oldpuzzle/OoTPuzzleRace10.png',
+		creator: 'Enterim',
+		number: '10'
+    },
+	{
+		title: 'Open The Map Chest in Spirit Temple',
+		image: 'oldpuzzle/OoTPuzzleRace11.png',
+		creator: 'MAXX',
+		number: '11',
+    },
+	{
+		title: 'Defeat Morpha',
+		image: 'oldpuzzle/OoTPuzzleRace12.jpg',
+		creator: 'TheOnlyOne',
+		number: '12',
+    },
+	{
+		title: 'Defeat Ganon',
+		image: 'oldpuzzle/OoTPuzzleRace13.png',
+		creator: 'MP16',
+		number: '13',
+    },
+	{
+		title: 'Beat Ganondorf',
+		image: 'oldpuzzle/OoTPuzzleRace14.jpg',
+		creator: 'Toolex',
+		number: '15',
+    
+	},
+	{
+		title: 'Obtain Double Defense',
+		image: 'oldpuzzle/OoTPuzzleRace15.png',
+		creator: 'Sockfolder',
+		number: '14',
+    },
+  ],
+  Impromptu: 
+  [
+  	{
+		title: 'Visit and obtain the reward from all six great fairy fountains.',
+		image: '',
+		creator: 'RingRush',
+		number: '1'
+    },
+	{
+		title: 'Get each dungeons main item.',
+		image: '',
+		creator: 'Cosmo',
+		number: '2'
+    },
+	{
+		title: 'Complety buy out the bombchu shop in the back alley.',
+		image: '',
+		creator: 'Cosmo',
+		number: '3'
+    },
+	{
+		title: 'Obtain the Frog HP.',
+		image: 'oldpuzzle/Impromptu-4.png',
+		creator: 'RingRush',
+		number: '4'
+    },
+	{
+		title: 'Fill up your whole equipment screen (swords, shields, boots, etc..). No RBA',
+		image: '',
+		creator: 'Remen',
+		number: '5'
+    },
+	{
+		title: 'Play Saria Song on the Boomerang (with Ocarina Items) next to the Mirror Shield chest.',
+		image: '',
+		creator: 'mzxrules',
+		number: '6'
+    },
+	{
+		title: 'Get all Deku Stick and Deku Nut upgrades and end at the Saria Song cutscene with the name "A55".',
+		image: '',
+		creator: 'Elminster ',
+		number: '7'
+    },
+	{
+		title: 'Obtain both the Bow and the Megaton Hammer as a child.',
+		image: '',
+		creator: 'pokey',
+		number: '8'
+    },
+	{
+		title: 'Impromptu #9',
+		image: '',
+		creator: 'darkeye14',
+		number: '9'
+    },
+	{
+		title: 'Eye Drops Impromptu',
+		image: 'oldpuzzle/Impromptu-15.png',
+		creator: 'darkeye14',
+		number: '15'
+    },
+	{
+		title: 'Blue Impromptu',
+		image: 'oldpuzzle/Impromptu-16.png',
+		creator: 'AniMeowzerz & darkeye14',
+		number: '16'
+    },
+	{
+		title: 'Pirate Impromptu',
+		image: 'oldpuzzle/Impromptu-17Final.png',
+		creator: 'Runnerguy2489',
+		number: '17'
+    },
+  ],
+}
