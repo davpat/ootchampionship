a84399dd1fd384ef73e9012b4743daf6aa1e76cd
unknown <zeldavslink@gmail.com>
2017-11-10 10:31:48 -0500

    cleared some stuff

--- public/bingoadventure/script/maps.js
+++ public/bingoadventure/script/maps.js
@@ -1,16 +1,38 @@
+var currentZone;
+var game;
+
 function loadGame(gameString)
 {
-    var game = JSON.parse(gameString);
-    setBackground(game.Scenes[0].BackgroundImage);
-    setDialogText(game.Scenes[0].Instances[0].Dialog.Text);
-    setCharacter(game.Scenes[0].Instances[0].Characters[1].CharacterSprites[0].Path);
-    setMusic(game.Scenes[0].BackgroundMusic);
+	currentZone = 0;
+    game = JSON.parse(gameString);
+    setZones(game.Zones[currentZone].BackgroundImage);
+	
 }
 var TextHelper = new textHelper();
-function setBackground(path)
+
+function setZones(path)
 {
     document.getElementById("scene").style.backgroundImage = "url('" + path + "')";
+	setZonesGoal(game.Zones[currentZone].Goal[0]);
 }
+
+function textHelper() { // constructor function
+    this.cptLines = 0; // Public variable
+}
+
+function setZonesGoal(text)
+{
+	var goals = document.createElement('div');
+	for(var k in text) 
+	{
+		var g = document.createElement('div');
+		g.id = k;
+		g.innerHTML = k + " : " + text[k];
+		goals.appendChild(g);
+	}
+	document.getElementById("goal").appendChild(goals);
+}
+
 function setMusic(path) {
     var audio = document.getElementById('audio');
     var source = document.getElementById('mp3Source');
@@ -18,61 +40,4 @@ function setMusic(path) {
 
     audio.load(); //call this to just preload the audio without playing
     audio.play(); //call this to play the song right away
-}
-
-function textHelper() { // constructor function
-    this.cptLines = 0; // Public variable
-}
-
-function setDialogText(text)
-{
-    //Replace . ? !  by $1| To keep then in the split.
-    var lines = text.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|");
-    var array = [];
-
-    lines.forEach(function (line) {
-        if (line.length < 280)
-        {
-            array.push(line);
-        }
-        else
-        {
-            array.push(line.substring(0, line.indexOf(" ", 300)));
-            array.push(line.substring(line.indexOf(" ", 300) + 1, line.length));
-        }
-    });
-
-    $('#dialogBox').click(function () {
-        if (TextHelper.cptLines != array.length)
-        {
-            $('#dialog').empty();
-            var spans = '<span>' + array[TextHelper.cptLines].split('').join('</span><span>') + '</span>' + "<span class=\"blink\">|</span>";
-            $(spans).hide().appendTo('.css-typing').each(function (i) {
-                $(this).delay(20 * i).css({
-                    display: 'inline',
-                    opacity: 0
-                }).animate({
-                    opacity: 1
-                }, 100);
-            });
-            TextHelper.cptLines++;
-        }
-    });  
-}
-
-
-function setCharacter(sprite)
-{
-    var addedCharacter = document.createElement("img");
-    var src = document.createAttribute("src");
-    src.nodeValue = sprite;
-    addedCharacter.classList.add("characterImage");
-    addedCharacter.attributes.setNamedItem(src);
-   
-    addedCharacter.addEventListener("dragstart", function (e)
-    {
-        e.preventDefault();
-        return false;
-    }, true);
-    document.getElementById("scene").appendChild(addedCharacter);
 }
